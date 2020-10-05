9fcdf7f7b60a7740f7a66de60e550be81a562860
Idkirsch <s195483@student.dtu.dk>
2020-10-05 11:00:57 +0200

    Har tilføjet ord fra DRs server

--- app/src/main/java/com/example/galgeleg/Spil.java
+++ app/src/main/java/com/example/galgeleg/Spil.java
@@ -4,15 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 public class Spil extends AppCompatActivity implements View.OnClickListener {
 
     Galgelogik galgelogik = new Galgelogik();
     TextView tekst, navneView;
     Button A, B;
+    //instantierer en forgrundstråd og en baggrundstråd
+    Executor backgroundThread = Executors.newSingleThreadExecutor();
+    Handler uiThread = new Handler(Looper.getMainLooper());
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +32,23 @@ public class Spil extends AppCompatActivity implements View.OnClickListener {
         System.out.println("Log");
 
         tekst = (TextView) findViewById(R.id.textView);
-        tekst.setText("Du skal gætte Ordet: " + galgelogik.getSynligtOrd());
+       // tekst.setText("Du skal gætte Ordet: " + galgelogik.getSynligtOrd());
+        tekst.setText("Henter ord fra DRs server ");
+
+        backgroundThread.execute(() ->{
+          try {
+              galgelogik.hentOrdFraDr();
+              uiThread.post(() -> {
+                  tekst.setText("Ordene blev korrekt hentet");
+              });
+          }catch (Exception e){
+              e.printStackTrace();
+              uiThread.post(() -> {
+                  tekst.setText("Ordene blev ikke hentet korretk" +e);
+              });
+          }
+        });
+
 
         navneView = findViewById(R.id.nameView);
         System.out.println(i.getStringExtra("spillerNavn"));
