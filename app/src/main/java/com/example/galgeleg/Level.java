6b52f9abd52eb41a5a46420fba94ccec79df6a49
Idkirsch <s195483@student.dtu.dk>
2021-01-03 15:20:19 +0100

    Ordene fra DR populater et listview i aktiviteten Level

--- app/src/main/java/com/example/galgeleg/Level.java
+++ app/src/main/java/com/example/galgeleg/Level.java
@@ -7,17 +7,22 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
 
+import java.util.List;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
 public class Level extends AppCompatActivity implements View.OnClickListener {
 
     Button videre;
+    ListView listen;
     String spillerNavn;
     HentOrd hentOrd = new HentOrd();
+
 
 
     Executor backgroundThread = Executors.newSingleThreadExecutor();
@@ -29,28 +34,44 @@ public class Level extends AppCompatActivity implements View.OnClickListener {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_level);
 
+
         videre = (Button) findViewById(R.id.knap_til_spil);
         videre.setOnClickListener(this);
+
+
 
         Intent i = getIntent();
         spillerNavn = i.getStringExtra("spillerNavn");
 
+        hentFraDR();
+
+        hentOrd.galgelogik.logStatus();
+
+        String items[] = new String[]{"Pis", "prut", "lort"};
+
+        //System.out.println("ordene: "+ hentOrd.galgelogik.muligeOrd);
+
+
+    }
+
+
+
+    private void hentFraDR() {
         backgroundThread.execute(() ->{
             try {
                 hentOrd.hentOrdFraDr();
                 uiThread.post(() -> {
                     System.out.println("Ord blev hentet fra DRs server");
 
+                    ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, hentOrd.galgelogik.muligeOrd);
+
+                    listen = (ListView) findViewById(R.id.list_view);
+                    listen.setAdapter(adapter);
                 });
             }catch (Exception e){
                 e.printStackTrace();
             }
         });
-
-
-        hentOrd.galgelogik.logStatus();
-
-
     }
 
 
@@ -59,7 +80,7 @@ public class Level extends AppCompatActivity implements View.OnClickListener {
         if(view == videre){
             Intent intent = new Intent(this, Spil.class);
             intent.putExtra("spillerNavn", spillerNavn);
-            intent.putExtra("valgtOrd", "Hejsa");
+            intent.putExtra("valgtOrd", "karl");
             this.startActivity(intent);
         }
     }
