2d5f3146fe612aa3ea7a017ec7cc91d4561ceff7
Idkirsch <s195483@student.dtu.dk>
2021-01-04 10:28:30 +0100

    ordet udskrives på taberskærmen

--- app/src/main/java/com/example/galgeleg/Tabt.java
+++ app/src/main/java/com/example/galgeleg/Tabt.java
@@ -6,11 +6,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class Tabt extends AppCompatActivity implements View.OnClickListener {
 
     Button forfra;
     String ord;
+    TextView tabt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +22,9 @@ public class Tabt extends AppCompatActivity implements View.OnClickListener {
         Intent i = getIntent();
         ord = i.getStringExtra("wordToGuess");
         System.out.println("tabt :-( ... ordet var: "+ ord);
+
+        tabt = (TextView) findViewById(R.id.duHarTabt);
+        tabt.append(" ordet var "+ord);
 
         forfra = (Button) findViewById(R.id.genstartTaber);
         forfra.setOnClickListener(this);
